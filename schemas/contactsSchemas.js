0744d111596aacd6762d18e82c0822a02429bcec
DanaKonyk <132483408+DanaKonyk@users.noreply.github.com>
2024-01-25 18:56:06 +0200

    db

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,18 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.number().required(),
+  favorite: Joi.boolean(),
+});
 
 export const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.number(),
+});
 
-})
+export const updateContactFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
