ac7ac5706c3d3de54687a795f3ace7f9684d9688
DanaKonyk <132483408+DanaKonyk@users.noreply.github.com>
2024-01-27 14:49:54 +0200

    id check

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -7,19 +7,20 @@ import {
   updateContact,
   updateContactFavoriteOption,
 } from "../controllers/contactsControllers.js";
+import isIdValid from '../middlewares/isIdValid.js'
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
 
-contactsRouter.get("/:id", getContactById);
+contactsRouter.get("/:id", isIdValid, getContactById);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", isIdValid, deleteContact);
 
 contactsRouter.post("/", createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", isIdValid, updateContact);
 
-contactsRouter.patch("/:id", updateContactFavoriteOption);
+contactsRouter.patch("/:id/favorite", isIdValid, updateContactFavoriteOption);
 
 export default contactsRouter;
